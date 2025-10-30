eebb1d93fbad72bcefee449406d3e07c015cb4a1
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-10-30 01:08:49 -0300

    Update wsgi.py

--- agendacontactos/wsgi.py
+++ agendacontactos/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agenda_contactos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agendacontactos.settings')
 
 application = get_wsgi_application()
